f85f4f05bd956695741cba585ba22e1654390c65
Nick Chiapputo <chiapputo.nick@gmail.com>
2021-10-26 16:45:16 -0500

    Moved training into separate function. Added early stopping. Modified optimizer.

--- nick/naive.py
+++ nick/naive.py
@@ -1,3 +1,4 @@
+from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.callbacks import EarlyStopping
 from sklearn.metrics import confusion_matrix
 import numpy as np
@@ -18,66 +19,53 @@ from models import create_lstm_model
 def main():
     # Define simulation parameters.
     data_length = 26        # Length to truncate/pad samples to.
-    epochs = 80             # Length of training.
-    num_iterations = 50     # Number of iterations of fit/test.
+    epochs = 100             # Length of training.
+    num_iterations = 15     # Number of iterations of fit/test.
     user_selections = []    # List to hold tuple of user splits (train/test/val)
+
 
     # Define naive model parameters.
     dropout_rate = 0.8
     lstm_units = 64
 
+    lr = 0.005
+    lstm_optimizer = Adam( learning_rate=lr, decay=1e-6 )
 
+    monitor = 'loss'
+    min_delta = 0.0001
+    patience = 3
+    earlystop_callback = EarlyStopping( monitor=monitor, min_delta=min_delta,
+                                  verbose=1, patience=patience )
+    lstm_callbacks = [earlystop_callback]
+    # lstm_callbacks = None
+
+    model_params = ( dropout_rate, lstm_units, data_length, lstm_optimizer )
+    fit_params = ( epochs, lstm_callbacks )
+
+
+    # Load data and trim/pad to set length.
     data = get_data( data_length=data_length )
 
+
+    # Create data structures to hold confusion matrix and loss/accuracy results
+    # for each iteration.
     cf_matrix_true = np.array( [] )
     cf_matrix_pred = np.array( [] )
     scores = []
 
+
     for i in range( num_iterations ):
-        # Select the training, test, and validation subjects.
-        # Get random ordering of subjects.
-        subject_list = np.random.permutation( 8 )
+        score, y_test, y_pred,\
+        train_val_test_splits = train_model( i, data, model_params, 
+                                             fit_params, verbose=1 )
 
-        # Select the second from last as validation and last user as test.
-        train_subjects = subject_list[ :-2 ].tolist()
-        test_subjects = subject_list[ -2:-1 ].tolist()
-        val_subjects = [ subject_list[ -1 ] ]
+        user_selections.append( train_val_test_splits )
 
-        user_selections.append( ( train_subjects, val_subjects, test_subjects ) )
-
-        print( f"============================================================\n"
-               f"Iteration {i+1}:\n"
-               f"    Train Subjects:      {user_selections[ i ][ 0 ]}\n"
-               f"    Validation Subjects: {user_selections[ i ][ 1 ]}\n"
-               f"    Test Subjects:       {user_selections[ i ][ 2 ]}\n" )
-
-        # Split the data into training, testing, and validation data and labels.
-        X_train, y_train, \
-        X_test, y_test,\
-        X_val, y_val = train_test_split( data,
-                                         train_subjects=train_subjects,
-                                         test_subjects=test_subjects,
-                                         val_subjects=val_subjects )
-        validation_data = (X_val, y_val) if X_val is not None else None
-
-        # Create the naive LSTM model without dropout.
-        # early_stopping = EarlyStopping( monitor='val_loss' )
-        lstm_naive_model = create_lstm_model( num_classes=20,
-                                              dropout=dropout_rate,
-                                              units=lstm_units,
-                                              data_length=data_length )
-
-        # Train the model on the training data.
-        lstm_naive_model.fit( X_train, y_train, epochs=epochs,
-                              validation_data=validation_data )
-
-        # Test the model to see how well we did.
-        score = lstm_naive_model.evaluate( X_test, y_test )
         scores.append( score )
 
         # Generate data for confusion matrix.
         cf_matrix_true = np.hstack( ( cf_matrix_true, y_test ) )
-        cf_matrix_pred = np.hstack( ( cf_matrix_pred, np.argmax( lstm_naive_model.predict( X_test ), axis=1 ) ) )
+        cf_matrix_pred = np.hstack( ( cf_matrix_pred, y_pred ) )
 
         # Logging output for current iteration.
         print( "test loss, test acc: ", score )
@@ -92,17 +80,93 @@ def main():
 
 
     # Save results.
-    save_results( scores, user_selections, cf_matrix, epochs,
-                  filename=f'results_{epochs}-epochs_{num_iterations}-iterations', 
-                  loc='./results/', filetype=0 )
+    # save_results( scores, user_selections, cf_matrix, epochs,
+    #               filename=f'results_{epochs}-epochs_{num_iterations}-iterations', 
+    #               loc='./results/', filetype=0 )
 
 
     # Plot the confusion matrix.
-    make_confusion_matrix( cf_matrix, categories=[ '01', '02', '03', '04', '05',
-                                                   '06', '07', '08', '09', '10',
-                                                   '11', '12', '13', '14', '15',
-                                                   '16', '17', '18', '19', '20' ],
-                           figsize=[8,8])
+    # make_confusion_matrix( cf_matrix, categories=[ '01', '02', '03', '04', '05',
+    #                                                '06', '07', '08', '09', '10',
+    #                                                '11', '12', '13', '14', '15',
+    #                                                '16', '17', '18', '19', '20' ],
+    #                        figsize=[8,8])
+
+
+def train_model( idx, data, model_params, fit_params, verbose=2 ):
+    """
+    Wrapper for training and testing model. Returns the testing loss and
+    accuracy and the test and predicted labels for the current iteration.
+
+    :param idx: Current iteration index.
+
+    :param data: Pandas dataframe of gesture data.
+
+    :param model_params: Tupled collection of LSTM model parameters. 
+    Contains: dropout_rate, lstm_units, data_length, lstm_optimizer
+
+    :param fit_params: Tupled collection of fit/train parameters.
+    Contains: epochs, lstm_callbacks.
+
+    :param verbose: Verbosity of output.
+    0 -- no output. 1 -- Only current iteration output. 2 -- Full.
+
+    :return: Returns tuple of test loss/accuracy, test labels, and predicted
+    labels.
+
+    """
+
+    # Get model parameters
+    dropout_rate, lstm_units, data_length, lstm_optimizer = model_params
+    epochs, lstm_callbacks = fit_params
+
+    # Select the training, test, and validation subjects.
+    # Get random ordering of subjects.
+    subject_list = np.random.permutation( 8 )
+
+    # Select the second from last as validation and last user as test.
+    train_subjects = subject_list[ :-3 ].tolist()
+    test_subjects = subject_list[ -3:-1 ].tolist()
+    val_subjects = [ subject_list[ -1 ] ]
+
+
+    if verbose > 0:
+        print( f"============================================================\n"
+               f"Iteration {idx+1}:\n"
+               f"    Train Subjects:      {train_subjects}\n"
+               f"    Validation Subjects: {val_subjects}\n"
+               f"    Test Subjects:       {test_subjects}\n" )
+
+    # Split the data into training, testing, and validation data and labels.
+    X_train, y_train, \
+    X_test, y_test,\
+    X_val, y_val = train_test_split( data,
+                                     train_subjects=train_subjects,
+                                     test_subjects=test_subjects,
+                                     val_subjects=val_subjects )
+    validation_data = (X_val, y_val) if X_val is not None else None
+
+    # Create the naive LSTM model without dropout.
+    lstm_naive_model = create_lstm_model( num_classes=20,
+                                          dropout=dropout_rate,
+                                          units=lstm_units,
+                                          data_length=data_length,
+                                          optimizer=lstm_optimizer )
+
+    # Train the model on the training data.
+    fit_verbose = 0 if verbose <= 1 else 2 if verbose == 2 else 1
+    lstm_naive_model.fit( X_train, y_train, epochs=epochs,
+                          callbacks=lstm_callbacks, verbose=verbose,
+                          validation_data=validation_data )
+
+    # Test the model to see how well we did.
+    score = lstm_naive_model.evaluate( X_test, y_test )
+
+
+    return score, y_test, \
+            np.argmax( lstm_naive_model.predict( X_test ), axis=1 ), \
+            ( train_subjects, val_subjects, test_subjects )
+            
 
 
 def get_data( data_length ):
